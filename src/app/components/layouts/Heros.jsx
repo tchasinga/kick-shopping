832d7f4779c7734add6e5986068cfa151f418a92
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-04 11:19:48 +0200

    new plaece is added

--- src/app/components/layouts/Heros.jsx
+++ src/app/components/layouts/Heros.jsx
@@ -2,20 +2,21 @@
 import React from 'react';
 import { useState , useEffect } from 'react';
 import Image from "next/image";
+import imgOne from '../'
 
 
 export default function Heros() {
     const [imageIndex, setImageIndex] = useState(0);
     const image = [
-        '../../img/AirJordan5Retro.png',
-        '../../img/AppleAirPods.png',
-        '../../img/Girl-jordan.png',
-        '../../img/Jordan1.png',
-        '../../img/MoreImages-fotor.png',
-        '../../img/TrendtationAirfor.png',
-        '../../img/YeezySlidesSlate.png',
-        '../../img/air-jordan-4-retro-se-craft.png',
+       
     ]
+
+    useEffect(() =>{
+        const intervalId = setInterval(() => {
+            setImageIndex((prevIndex) => (prevIndex + 1) % image.length);
+        }, 5000)
+        return () => clearInterval(intervalId);
+    }, [image.length])
 
   return (
     <div>
@@ -24,6 +25,10 @@ export default function Heros() {
         <p>Every outfit can look great with a good shoes now take your shopping...</p>
         <p>Life is short buy a shoes</p>
         <button>SHOP NOW</button>
+
+        <div className="">
+            <Image  src={image[imageIndex]} width={500} height={500} alt="air-jordan-4-retro-se-craft.png"/>
+        </div>
     </div>
   )
 }
