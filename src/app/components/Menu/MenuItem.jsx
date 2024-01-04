cfa9a334aa681f9a82984e1a709f441fa21fa9c0
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-04 13:01:30 +0200

    New commit

--- src/app/components/Menu/MenuItem.jsx
+++ src/app/components/Menu/MenuItem.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function MenuItem() {
   return (
-    <div className="bg-gray-300 p-4  rounded-lg text-center">
+    <div className="bg-gray-200 p-4  rounded-lg text-center">
     <Image  src={imgTwo}  alt="air-jordan-4-retro-se-craft.png"/> 
     <h3 className='text-sm'>Air Jordan 4 Retro SE</h3>
     <p className='text-xs'>Craft the great shoes all time </p>
