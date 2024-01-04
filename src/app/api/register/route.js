3386a3bd3f8f85aa3b168bf36482b40da04353aa
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-04 15:40:05 +0200

    New code is added

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,9 +1,11 @@
-import React from 'react'
 
 export default function POST() {
+    
+
+
   return (
     <div>
-        
+
     </div>
   )
 }
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,15 +1,23 @@
+"use client"
+
 import React from 'react'
 import { TextField ,Button , Tooltip} from '@mui/material';
+import {useState} from 'react'
+
 
 export default function Register() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('') 
+ 
   return (
     <section className='mt-8'>
         <h1 className='text-center text-slate-900 text-4xl font-medium'>Register</h1> 
 
         <form className=' max-w-xl mx-auto mt-6'>
          <div className="w-full flex flex-col">
-         <TextField type='email' variant='outlined' id="" label="enter your email here" helperText='your email must have @ property' required/>
-          <TextField type='password' variant='outlined' id="" label="enter your password here" helperText='your password must have 8 characters' required/>
+         <TextField type='email' variant='outlined' value={email} onChange={ev => setEmail(ev.target.value)} id="" label="enter your email here" helperText='your email must have @ property' required/>
+          <TextField type='password' variant='outlined' value={password} onChange={ev =>setPassword(ev.target.value)} id="" label="enter your password here" helperText='your password must have 8 characters' required/>
           <Button type='submit' className='w-2/3' variant='outlined'>register</Button>
          </div>
          <div className='my-4 text-center text-slate-600'>or login with provider</div>
