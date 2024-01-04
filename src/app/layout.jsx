94ec64d1af4e61c9b0997ddd3435952aafcf9cb9
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-04 10:25:58 +0200

    new commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-         <main className='max-w-6xl border mx-auto'>
+         <main className='max-w-6xl  mx-auto p-4'>
          {children}
          </main>
         </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,19 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-     <h1 className=''>New code is added...</h1>
-    </main>
-  )
+    <>
+      <header className="flex items-center justify-between">
+        <Link className="text-slate-900 text-2xl font-medium" href="/">KICK SHOP</Link>
+        <nav className="flex gap-2 items-center">
+          <Link href={""}>Home</Link>
+          <Link href={""}>Shop</Link>
+          <Link href={""}>About</Link>
+          <Link href={""}>Contact</Link>
+          <Link href={""}>Loging</Link>
+        </nav>
+      </header>
+    </>
+  );
 }
