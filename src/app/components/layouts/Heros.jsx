65ff57529fd2e0cab304b9d01ab24ba120196b7e
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-04 10:53:27 +0200

    New code is added

--- src/app/components/layouts/Heros.jsx
+++ src/app/components/layouts/Heros.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Heros() {
   return (
     <div>
-        
+        <h1>KICK SHOPPING</h1>
+        <p>The best seller of sneakers on hearth</p>
+        <p>Every outfit can look great with a good shoes now take your shopping...</p>
+        <p>Life is short buy a shoes</p>
+        <button>SHOP NOW</button>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "./components/layouts/Header";
+import Heros from "./components/layouts/Heros";
 
 
 export default function Home() {
   return (
     <> 
     <Header/>
+    <Heros/>
     </>
   );
 }
