96db84239e3cb7bf20e6059a2464e71f8db62b93
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-05 18:41:39 +0200

    some Updateing

--- src/app/api/profiles/route.js
+++ src/app/api/profiles/route.js
@@ -9,9 +9,10 @@ export default async function  PUT(req) {
   const myLinks = "mongodb+srv://tchasingajacques:jack202050081@kick-shopping.qgo3vzq.mongodb.net/kick-shopping?retryWrites=true&w=majority"
   mongoose.connect(process.env.MONGO_URL || myLinks);
   const session = await getServerSession(authOptions);
+  const email = session.user.email;
   
   if('name' in data){
-   
+   await User.updateOne({email}, {$set: {name: data.name}})
   } 
   return Response.json(true);
 }
