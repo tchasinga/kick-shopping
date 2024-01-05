158abd72edde9d50301100aee30e405b2f156cbe
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-05 12:12:31 +0200

    new code commited

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,25 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler  = NextAuth({
+const mySecret = "balolebwamitchasingathebest";
+
+const handler = NextAuth({
+  // Configure one or more authentication providers
+  secret: process.env.NEXTAUTH_SECRET || mySecret,
   providers: [
     CredentialsProvider({
       name: "Credentials",
-     credentials: {
-          username: { label: "enter your email here", type: "email", placeholder: "tchasingajacque@gmail.com" },
-          password: { label: "enter your password here", type: "password" }
-        },
-      async authorize(credentials , req){
-        console.log({credentials})
+      id: "credentials",
+      credentials: {
+        username: { label: "enter your email here", type: "email", placeholder: "tchasingajacque@gmail.com" },
+        password: { label: "enter your password here", type: "password" }
+      },
+      async authorize(credentials, req) {
+        console.log({ credentials });
         return null;
       }
     })
-      
   ]
-})
+});
 
-export { handler as GET , handler as POST}
+export { handler as GET, handler as POST };
