597ae58bab65f59e9fb7b895ccc75fff3e30e118
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-05 12:06:18 +0200

    New code is added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,21 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers:  [
+const handler  = NextAuth({
+  providers: [
     CredentialsProvider({
-       
-        name: "Credentials",
-
-        credentials: {
+      name: "Credentials",
+     credentials: {
           username: { label: "enter your email here", type: "email", placeholder: "tchasingajacque@gmail.com" },
           password: { label: "enter your password here", type: "password" }
         },
-        async authorize(credentials, req) {
-          // Add logic here to look up the user from the credentials supplied
-          const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-    
-          if (user) {
-            // Any object returned will be saved in `user` property of the JWT
-            return user
-          } else {
-            // If you return null then an error will be displayed advising the user to check their details.
-            return null
-    
-            // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-          }
-        }
-      })
-    // ...add more providers here
-  ],
-}
+      async authorize(credentials , req){
+        console.log({credentials})
+        return null;
+      }
+    })
+      
+  ]
+})
 
-export default NextAuth(authOptions)
+export { handler as GET , handler as POST}
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import { TextField ,Button , Tooltip} from '@mui/material';
+import Link from 'next/link'
+// import { signIn } from '../api/auth/[...nextauth]/route.ts'
+import { signIn } from 'next-auth/react'; // Import signIn from next-auth
+
+
 
 export default function Loginpages() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('') 
+    const [loginInProgress, setLoginInProgress] = useState(false);
+    
+     const handlerFormSubmit = async (ev) => {
+        ev.preventDefault();
+        setLoginInProgress(true);
+        await signIn('Credentials')
+        setLoginInProgress(false);
+     }
+  
+
   return (
    <section className='mt-8'>
-    <h1 className='text-center text-slate-900 text-4xl font-medium'>Register</h1> 
+    <h1 className='text-center text-slate-900 text-4xl font-medium'>Login now</h1> 
+
+    <form className=' max-w-xl mx-auto mt-6' onSubmit={handlerFormSubmit}>
+         <div className="w-full flex flex-col">
+         <TextField type='email' name='email' variant='outlined' disabled={loginInProgress} id="" label="enter your email here" helperText='your email must have @ property' />
+          <TextField type='password' name='password' variant='outlined' disabled={loginInProgress} id="" label="enter your password here" helperText='your password must have 8 characters' />
+          <Button type='submit' className='w-2/3' disabled={loginInProgress} variant='outlined'>Login now</Button>
+         </div>
+         <div className='my-4 text-center text-slate-600'>Or login with provider</div>
+          <div className="text-center border p-3 rounded-2xl font-medium">
+          <button className=''>Login with google</button>
+         </div>
+         <div className='flex  gap-2 text-xs mt-3'>
+            <p>Don't have an account</p>
+            <Link className='text-green-800 font-extrabold' href={`/register`}>register &raquo;</Link>
+          </div>
+    </form>
    </section>
   )
 }
