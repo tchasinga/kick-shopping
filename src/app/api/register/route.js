95a2118ee4f86701b16ac1e717972f47f10b56cf
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-05 09:30:13 +0200

    new side of myLinks.mongodbmyLinks.mongodbmyLinks.mongodb

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -3,8 +3,9 @@ import User from "../models/user.model.js";
 
 export async function POST(req) {
   const body = await req.json();
+  const myLinks = "mongodb+srv://tchasingajacques:jack202050081@kick-shopping.qgo3vzq.mongodb.net/kick-shopping?retryWrites=true&w=majority"
 
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect(process.env.MONGO_URL || myLinks);
   const newUser = await User.create(body); // Ensure 'User' is defined correctly
   return Response.json(newUser);
 }
