3637f1053e45e95998dc388ee3d5692a5ea8f8b3
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-06 10:48:17 +0200

    new code is added

--- src/app/api/profiles/route.js
+++ src/app/api/profiles/route.js
@@ -4,7 +4,7 @@ import User from "../models/user.model.js";
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../auth/[...nextauth]/route.js';
 
-export default async function  PUT(req) {
+export default async function  put(req) {
   const data = await req.json();
   const myLinks = "mongodb+srv://tchasingajacques:jack202050081@kick-shopping.qgo3vzq.mongodb.net/kick-shopping?retryWrites=true&w=majority"
   mongoose.connect(process.env.MONGO_URL || myLinks);
